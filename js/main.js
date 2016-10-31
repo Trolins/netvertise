f8f3df41e8f3cf497e863efed3cd84ad02ce856d
Sinck <trolins3@gmail.com>
2016-10-31 13:11:18 +0200

    finish commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
 requirejs.config({
-	urlArgs: "vol.1.11",
-	waitSeconds: 2000,
-    baseUrl: 'js/',
+	urlArgs: "vol.1.12",
+	waitSeconds: 40,
 	paths: {
 		jquery: 'vendors/jquery-3.0.0.min',
 		angular: 'vendors/angular.min',
 		bootstrap: 'vendors/bootstrap.min',
-
 	},
+	waitSeconds: 40,
+    baseUrl: 'js/',
     priority: [
-        "jquery",
-        "angular"
+        'jquery',
+        'angular'
     ]
 });
 
